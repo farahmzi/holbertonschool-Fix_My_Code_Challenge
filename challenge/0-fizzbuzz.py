39d519d7813b85389be5170d0effb027fb9c35a3
farahmzi <farahalmzini@gmail.com>
2025-12-06 19:38:46 +0000

    Fix FizzBuzz: correct logic and make script executable

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
